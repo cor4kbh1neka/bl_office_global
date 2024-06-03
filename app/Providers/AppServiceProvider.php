9efbe286ac70b98571bfbb67510a09284bf9a860
wantos <irwanzzz65@gmail.com>
2024-06-03 17:02:20 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -153,6 +153,8 @@ class AppServiceProvider extends ServiceProvider
     }
     public function userAndUserAccess()
     {
+        Cache::flush();
+
         $user = auth()->user();
         $cacheKey = 'user_access_' . $user->id;
 

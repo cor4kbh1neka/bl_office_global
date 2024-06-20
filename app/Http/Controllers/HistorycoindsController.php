91e40b87bb93ccf0108a4a98e16d03d9950fbe10
wantos <irwanzzz65@gmail.com>
2024-06-20 17:57:05 +0700

    update

--- app/Http/Controllers/HistorycoindsController.php
+++ app/Http/Controllers/HistorycoindsController.php
@@ -102,9 +102,9 @@ class HistorycoindsController extends Controller
         $tglsampai = $request->input('tglsampai');
 
         // if ($tgldari >= $semingguYangLalu && $tgldari <= $hariIni && $tglsampai >= $semingguYangLalu && $tglsampai <= $hariIni && $tgldari <= $tglsampai) {
-            $crot = $this->filterAndPaginate(9999999999999999);
-            $data = $crot->getCollection();
-            return Excel::download(new DepoWdExport($data), 'Historycoin.xlsx');
+        $crot = $this->filterAndPaginate(9999999999999999);
+        $data = $crot->getCollection();
+        return Excel::download(new DepoWdExport($data), 'Historycoin.xlsx');
         // } else {
         //     return redirect('historycoinds')->with('gagalTarikData', 'Harap masukkan rentang tanggal dalam 7 hari terakhir');
         // }

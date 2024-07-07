fa469f0ee85572b4140d0837b9565a21cfbc3e36
wantos <irwanzzz65@gmail.com>
2024-07-07 11:30:54 +0700

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -852,42 +852,31 @@ class ApiController extends Controller
         $startDate = $request->startDate;
         $endDate = $request->endDate;
 
-        $results = [
-            "result" => [],
-            "serverId" => "YY-production",
-            "error" => [
-                "id" => 0,
-                "msg" => "No Error"
-            ]
+        $data = [
+            'username' => env('UNIX_CODE') . $username,
+            'portfolio' => $portfolio,
+            'startDate' => $startDate . 'T00:00:00.540Z',
+            'endDate' => $endDate . 'T23:59:59.540Z',
+            'companyKey' => env('COMPANY_KEY'),
+            'language' => 'en',
+            'serverId' => env('SERVERID')
+
         ];
+        $apiUrl = env('BODOMAIN') . '/web-root/restricted/report/get-bet-list-by-modify-date.aspx';
 
-        if ($portfolio != 'SeamlessGame') {
+        $response = Http::post($apiUrl, $data);
+        $results = $response->json();
 
-            $data = [
-                'username' => env('UNIX_CODE') . $username,
-                'portfolio' => $portfolio,
-                'startDate' => $startDate . 'T00:00:00.540Z',
-                'endDate' => $endDate . 'T23:59:59.540Z',
-                'companyKey' => env('COMPANY_KEY'),
-                'language' => 'en',
-                'serverId' => env('SERVERID')
-
-            ];
-            $apiUrl = env('BODOMAIN') . '/web-root/restricted/report/get-bet-list-by-modify-date.aspx';
-
-            $response = Http::post($apiUrl, $data);
-            $results = $response->json();
-
-            if ($results["error"]["id"] == 0) {
-                $results = $results['result'];
-                foreach ($results as &$d) {
-                    $d['orderTime'] = Carbon::parse($d['orderTime'])->addHours(11)->toDateTimeString();
-                    $d['modifyDate'] = Carbon::parse($d['modifyDate'])->addHours(11)->toDateTimeString();
-                    $d['settleTime'] = Carbon::parse($d['settleTime'])->addHours(11)->toDateTimeString();
-                    $d['winLostDate'] = Carbon::parse($d['winLostDate'])->addHours(11)->toDateTimeString();
-                }
+        if ($results["error"]["id"] == 0) {
+            $results = $results['result'];
+            foreach ($results as &$d) {
+                $d['orderTime'] = Carbon::parse($d['orderTime'])->addHours(11)->toDateTimeString();
+                $d['modifyDate'] = Carbon::parse($d['modifyDate'])->addHours(11)->toDateTimeString();
+                $d['settleTime'] = Carbon::parse($d['settleTime'])->addHours(11)->toDateTimeString();
+                $d['winLostDate'] = Carbon::parse($d['winLostDate'])->addHours(11)->toDateTimeString();
             }
         }
+
         return $results;
     }
 

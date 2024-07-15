ebf2a8bc69295b9c90b933149b0847fccd844a6c
wantos <irwanzzz65@gmail.com>
2024-07-15 20:15:24 +0700

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -496,7 +496,7 @@ class ApiController extends Controller
             DB::commit();
             if ($dataWD) {
                 $prosesWD = $this->ApiProsesWithdraw($txnid, $dataWD);
-                if ($prosesWD["error"]["id"] === 0) {
+                if ($prosesWD["error"]["id"] !== 0) {
                     ListError::create([
                         'fungsi' => 'ApiWithdraw',
                         'pesan_error' => $prosesWD["error"]["id"],
@@ -513,10 +513,10 @@ class ApiController extends Controller
                 }
             }
 
-            // return response()->json([
-            //     'status' => 'Success',
-            //     'message' => 'Withdrawal sedang diproses'
-            // ]);
+            return response()->json([
+                'status' => 'Success',
+                'message' => 'Withdrawal sedang diproses'
+            ]);
         } catch (\Exception $e) {
             DB::rollBack();
             return response()->json([

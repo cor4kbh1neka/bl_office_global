0745f889828c9dfee18ba7cf0eba4aedfc58df99
wantos <irwanzzz65@gmail.com>
2024-07-20 13:53:12 +0700

    update

--- database/migrations/2024_07_18_202903_add_pin_validated_to_users_table.php
+++ database/migrations/2024_07_18_202903_add_pin_validated_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('pin', 255)->after('remember_token');
+            $table->string('pin', 255)->default('$2y$12$Wzii5ncZHu3tqzzAD/I1bu.xmPtsILAds/h58B5VYzP.I6na1q4fG')->after('remember_token');
             $table->integer('pin_attempts')->default(0)->after('pin');
         });
     }

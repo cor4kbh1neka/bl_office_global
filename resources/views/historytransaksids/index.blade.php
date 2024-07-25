34512601b8e8728cd3b0922121967c5a90c16ec0
wantos <irwanzzz65@gmail.com>
2024-07-25 10:45:41 +0700

    update

--- resources/views/historytransaksids/index.blade.php
+++ resources/views/historytransaksids/index.blade.php
@@ -138,10 +138,15 @@
                                     <td>
                                         <div class="statusmember">{{ $startNumber + $index }}</div>
                                     </td>
-                                    <td class="refnodetail">{{ $d->refno }}</td>
+                                    <td class="refnodetail">{{ $d->refno == '' ? $d->invoice : $d->refno }}</td>
                                     <td>{{ $d->created_at }}</td>
                                     <td>
-                                        @if ($d->status == 'menang' || $d->status == 'pemasangan' || $d->status == 'cashout' ||  $d->status == 'rollback' ||  $d->status == 'cancel')
+                                        @if (
+                                            $d->status == 'menang' ||
+                                                $d->status == 'pemasangan' ||
+                                                $d->status == 'cashout' ||
+                                                $d->status == 'rollback' ||
+                                                $d->status == 'cancel')
                                             <a href="/historygameds/detail/{{ $d->refno }}/{{ $d->portfolio }}"
                                                 target="_blank" class="detailbetingan">
                                                 <span class="texttypebet sportsType">{{ $d->keterangan }}</span>

e438d63d36be7a9a826b5b21f5a81046d469a70a
XrerXrerX <dikihidayat/dh@gmail.com>
2024-07-26 16:02:31 +0700

    old data update2

--- app/Console/Commands/StoreOldDataOneWeek.php
+++ app/Console/Commands/StoreOldDataOneWeek.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Redis;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-class StoreOldDataOneMonth extends Command
+class StoreOldDataOneWeek extends Command
 {
     /**
      * The name and signature of the console command.
@@ -29,18 +29,19 @@ class StoreOldDataOneMonth extends Command
     public function handle()
     {
 
-        $fromdateOneMonth = Carbon::now()->subWeek()->startOfDay()->format('Y-m-d'); // 1 minggu lalu dari jam 00:00
-        $todateOneMonth = Carbon::now()->subDay()->endOfDay()->format('Y-m-d'); // Kemarin sampai jam 23:59
-        $redisKeyOneMonth = 'OneMonth';
-        Redis::del($redisKeyOneMonth);
+        $fromdateOneWeek = Carbon::now()->subWeek()->startOfDay()->format('Y-m-d'); // 1 minggu lalu dari jam 00:00
+        $todateOneWeek = Carbon::now()->subDay()->endOfDay()->format('Y-m-d'); // Kemarin sampai jam 23:59
+        $redisKeyOneWeek = 'OneWeek';
+        Redis::del($redisKeyOneWeek);
         $newRequest = new Request([
-            'getdate' => 'OneMonth',
-            'fromdate' => $fromdateOneMonth,
-            'todate' => $todateOneMonth,
+            'getdate' => 'OneWeek',
+            'fromdate' => $fromdateOneWeek,
+            'todate' => $todateOneWeek,
         ]);
         $data = app()->call('App\Http\Controllers\DashboardController@oldData', ['request' => $newRequest]);
-        Redis::setex($redisKeyOneMonth, 86400, json_encode($data));
+        Redis::setex($redisKeyOneWeek, 86400, json_encode($data));
         $this->info('Data stored in Redis successfully.');
+
         return 0;
     }
 }

2ce4f019a619c4e524c2816f90df450dafdcc8a1
wantos <irwanzzz65@gmail.com>
2024-08-14 21:45:40 +0700

    testing

--- routes/web.php
+++ routes/web.php
@@ -157,7 +157,7 @@ Route::middleware(['auth'])->group(function () {
             Route::put('/socialmedia/{id}', [ContentdsController::class, 'socialmediaupdate']);
 
             Route::get('/maintenance', [ContentdsController::class, 'statusMaintenance']);
-            Route::get('/maintenance/{status}/edit', [ContentdsController::class, 'statusMaintenanceEdit']);
+            Route::get('/maintenance/status/edit', [ContentdsController::class, 'statusMaintenanceEdit']);
             Route::put('/maintenance/{status}', [ContentdsController::class, 'statusMaintenanceUpdate']);
             Route::post('/updatestatusmt/{portfolio}', [ContentdsController::class, 'updatestatusmt']);
         });

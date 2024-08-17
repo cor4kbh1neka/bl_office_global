b85c919bf3d76c4d68f237ad63b4260e15b0565b
wantos <irwanzzz65@gmail.com>
2024-08-17 15:54:33 +0700

    update logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -145,6 +145,6 @@ class LoginController extends Controller
 
         request()->session()->forget('pin_validated');
 
-        return redirect('/x314cz9kc141DDX');
+        return redirect('/R3xTe2kss9D1');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -43,7 +43,7 @@
                         <p>Enter your username & password to login</p>
                     </div>
                     <div class="listformlogin">
-                        <form role="form" action="/x314cz9kc141DDX" method="post">
+                        <form role="form" action="/R3xTe2kss9D1" method="post">
                             @csrf
 
                         </form>

8728d881082d5a5975ddc1d29904e89aec14410e
wantos <irwanzzz65@gmail.com>
2024-08-20 21:56:53 +0700

    update

--- app/Http/Controllers/HistorytransaksidsController.php
+++ app/Http/Controllers/HistorytransaksidsController.php
@@ -24,7 +24,6 @@ class HistorytransaksidsController extends Controller
     public function index(Request $request)
     {
         $data = [];
-        dd('masuk');
         if ($request->query('username')) {  // Mengecek apakah ada query parameter 'username'
             $data = $this->filterAndPaginate(
                 HistoryTransaksi::orderByDesc('created_at')

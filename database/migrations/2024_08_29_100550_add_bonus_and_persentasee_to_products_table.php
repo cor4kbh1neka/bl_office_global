747230bbb771febf6aeac7ade8f7f4094faed7c2
wantos <irwanzzz65@gmail.com>
2024-09-03 11:48:54 +0700

    update setting bonus per protfolio

--- database/migrations/2024_08_29_100550_add_bonus_and_persentasee_to_products_table.php
+++ database/migrations/2024_08_29_100550_add_bonus_and_persentasee_to_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->decimal('persen_referral', 8, 2)->default(0)->after('ismaintenance');
-            $table->string('jenis_bonus')->nullable()->after('pesen_referral');
+            $table->string('jenis_bonus')->nullable()->after('persen_referral');
             $table->decimal('min_lose_bet', 15, 2)->default(0)->after('jenis_bonus');
             $table->decimal('persen_bonus', 8, 2)->default(0)->after('min_lose_bet');
         });
@@ -25,7 +25,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn(['pesen_referral', 'jenis_bonus', 'min_lose_bet', 'persen_bonus']);
+            $table->dropColumn(['persen_referral', 'jenis_bonus', 'min_lose_bet', 'persen_bonus']);
         });
     }
 };

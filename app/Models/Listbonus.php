50951431a763735ca0600f644715e62ba8001cc5
wantos <irwanzzz65@gmail.com>
2024-09-10 14:07:57 +0700

    update

--- app/Models/Listbonus.php
+++ app/Models/Listbonus.php
@@ -12,6 +12,6 @@ class Listbonus extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['no_invoice', 'periodedari', 'periodesampai', 'jenis_bonus', 'kecuali', 'total', 'status', 'processed_by'];
+    protected $fillable = ['no_invoice', 'periodedari', 'periodesampai', 'jenis_bonus', 'detail_bonus', 'kecuali', 'total', 'status', 'processed_by'];
     protected $table = 'listbonus';
 }

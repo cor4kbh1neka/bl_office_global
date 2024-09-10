51f2a6fbe01d2395f73438999a1be6fcebc06a84
wantos <irwanzzz65@gmail.com>
2024-09-10 12:48:10 +0700

    update

--- app/Http/Controllers/BonusdsController.php
+++ app/Http/Controllers/BonusdsController.php
@@ -168,7 +168,7 @@ class BonusdsController extends Controller
                 }
 
                 $total = $bonus == 'cashback' ? $result->totalwinloss : $result->totalstake;
-                dd($total);
+                dd($result);
                 if ($bonus == 'cashback') {
                     if ($total <= ($mBonus['min_lose_bet'] * -1)) {
                         $result->totalbonus = (abs($total) * $mBonus['persen_bonus']) / 100;

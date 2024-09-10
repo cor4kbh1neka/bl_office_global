13db0a60a63beb2e024a25dd072111591a0d6493
wantos <irwanzzz65@gmail.com>
2024-09-10 19:32:43 +0700

    update

--- app/Http/Controllers/BonusdsController.php
+++ app/Http/Controllers/BonusdsController.php
@@ -240,7 +240,7 @@ class BonusdsController extends Controller
                             if ($balance) {
                                 $balance = $balance->amount;
                             }
-                            $keterangan = $d['productsname'];
+                            $keterangan = 'B. '$d['productsname'];
                             $this->createDepoWD($d['username'], $nominalBonus, $keterangan, 'DPM', $txnid, $balance, Auth::user()->username, 1);
 
                             // 3. Process balance

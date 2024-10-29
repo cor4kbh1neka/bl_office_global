153eee017419f67c2ac826cdd2a0e0620be6f40b
wantos <irwanzzz65@gmail.com>
2024-10-29 19:16:39 +0700

    edit command fetch data very old

--- app/Console/Commands/CommandFetchingVeryOldData.php
+++ app/Console/Commands/CommandFetchingVeryOldData.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
-class CommandFetchingOldData extends Command
+class CommandFetchingVeryOldData extends Command
 {
     /**
      * The name and signature of the console command.
